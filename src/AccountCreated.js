1ac04b8014a1594228acceda7e14e008d6ce65d2
AnshikaOel <119701265+AnshikaOel@users.noreply.github.com>
2024-01-26 19:25:36 +0530

    Update AccountCreated.js

--- src/AccountCreated.js
+++ src/AccountCreated.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link ,useLocation} from 'react-router-dom'
 
 export default function AccountCreated() {
+  const location = useLocation();
+  const {state:{info}}=location
+  useEffect(()=>{
+    console.log('Recieved props',info)
+  },[info])
+  let data=JSON.stringify(info)
+  console.log("this is accountl"+data)
   return (
     <div>
       <div className='g-3 col-md-6 box'>
         <h1>Account Created successfully</h1>
         <br></br>
-        <button className='btn btn-primary'><Link to={{pathname:'/feed'}}>Create Your First Post</Link></button>
+        <button className='btn btn-primary'><Link to={{pathname:'/feed'}} className="linkStyle">Create Your First Post</Link></button>
       </div>
     </div>
   )
-}
+} 
