7e8cbad1a3a182d6023a1eec184448a81c0950b2
AnshikaOel <119701265+AnshikaOel@users.noreply.github.com>
2024-01-26 19:27:53 +0530

    Update OTP.js

--- src/OTP.js
+++ src/OTP.js
@@ -1,19 +1,74 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate,useLocation } from 'react-router-dom';
+
 
 export default function OTP() {
+  const location = useLocation();
+  const navigate=useNavigate();
+  const  [userOTP,setuserOTP]=useState('')
+  const {state:{info}}=location
+
+  useEffect(()=>{
+    console.log('Recieved props',info)
+  },[info])
+  
+  const handleregistration=async(e)=>{
+    console.log(info)
+    try{
+    const response=await fetch("http://localhost:5000/userInfo",{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json',
+        },
+    body:JSON.stringify(info),
+    })
+ if(response.ok){
+  console.log("Data saved succcessfully on server")
+ }else{
+  navigate('/registration')
+  console.log("Failed to save data server"+response.status)
+ }
+}catch(err){
+    console.error(err)
+}
+}
+  
+  
+  const SubmitOTP=async(e)=>{
+
+    e.preventDefault();  
+
+    try{
+      const response=await fetch('http://localhost:5000/verifyOTP',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',    
+        },
+        body:JSON.stringify({userOTP})
+      })
+      const responseData=await response.json()
+      if(responseData.message=='true'){
+        console.log("this is otp"+responseData.message)
+        handleregistration()
+        navigate('/AccountCreated',{state:{info}})
+      }else{
+        navigate('/registration')
+      }
+    }catch(error){
+      console.error('Error sending data ',error)
+    }
+
+  }
   return (
     <div>
+    <form className="row g-3" action="/submit" method="post" onSubmit={SubmitOTP}>
       <div className=' g-3 col-md-6 box ' >
       <label for="otp"><h1>Enter the OTP send on Your Email</h1></label>
-      <input type='number' minLength={1} maxLength={1} className='otpbox'></input>
-      <input type='number' minLength={1} maxLength={1} className='otpbox'></input>
-      <input type='number' minLength={1} maxLength={1} className='otpbox'></input>
-      <input type='number' minLength={1} maxLength={1} className='otpbox'></input>
+      <input type='number' minLength={4} maxLength={4} className='otpbox' value={userOTP} onChange={(e)=>{setuserOTP(e.target.value)}}></input>
       <br></br>
-      <button type='submit' className='btn btn-primary submitText' ><Link to={{pathname:'/AccountCreated'}}>Continue</Link></button>
+      <button type='submit' className='btn btn-primary submitText' >Continue</button>
       </div>
-      
+    </form>  
     </div>
   )
 } 
