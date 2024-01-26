eca709702c9751e3e498fc12ea7c56fa55a4aa0c
AnshikaOel <119701265+AnshikaOel@users.noreply.github.com>
2024-01-26 19:27:10 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
     <div>
-      <h1>registration Done</h1>
+      <div className=' g-3 col-md-6 box '>
+      <h1>Start Posting ANONYMOUSLY where NO One is judging </h1><br></br>
+      <center>
+      <button  className='btn btn-primary submitText'><Link to='/registration' className="linkStyle">Create Account</Link></button>
+      
+      <br></br><br></br>
+      Already have a Account
+      <br></br>
+      <button  className='btn btn-primary submitText'><Link to='/login' className="linkStyle">Login</Link></button>
+      </center>
+      </div>
     </div>
   )
-}
+} 
  
