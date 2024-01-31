54986c98029721bc52b63a38e865a64e03daaabe
AnshikaOel <119701265+AnshikaOel@users.noreply.github.com>
2024-01-31 16:06:27 +0530

    Update AccountCreated.js

--- src/AccountCreated.js
+++ src/AccountCreated.js
@@ -1,20 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState} from 'react'
 import { Link ,useLocation} from 'react-router-dom'
+import image from "./shareThought.jpg"
 
 export default function AccountCreated() {
   const location = useLocation();
-  const {state:{info}}=location
+  let {state:{id}}=location
   useEffect(()=>{
-    console.log('Recieved props',info)
-  },[info])
-  let data=JSON.stringify(info)
-  console.log("this is accountl"+data)
+    console.log('Recieved props in account created',id) 
+  },[id])
+   id=JSON.stringify(id)
+   id=JSON.parse(id)
+  //  console.log("THis sis account created so "+id)
+   const [fullname,setfullname]=useState('')
+   // calling function for the user name
+   const userName=async()=>{
+    let nameUser
+    try{
+      const response=await fetch('http://localhost:5000/userName',{
+        method:'POST',
+        headers:{'Content-Type':'application/json',
+      },
+      body:JSON.stringify({id}),
+    })
+    console.log("this is account created ")
+    if(response.ok)
+    {
+      let a=await response.json()
+       nameUser=a.data.fullname
+       setfullname(nameUser)
+      console.log("yaayy..we got the user name"+nameUser+"--"+typeof(nameUser))
+    }else{
+      console.log("Some error occured in getting the user name")
+    }
+   }catch(err){
+    console.error(err)
+   }
+
+  }
+  userName()
+  console.log("jbvkj"+fullname)
   return (
     <div>
-      <div className='g-3 col-md-6 box'>
+      <img src={image} alt='background-image'/>
+      <div id='d'>
         <h1>Account Created successfully</h1>
+        <h2 className='accnt_username'>{fullname}</h2>
         <br></br>
-        <button className='btn btn-primary'><Link to={{pathname:'/feed'}} className="linkStyle">Create Your First Post</Link></button>
+        <center><button className='btn btn-primary'><Link to={{pathname:'/feed',state:{data:id}}}className="linkStyle">Create Your First Post</Link></button></center>
       </div>
     </div>
   )
