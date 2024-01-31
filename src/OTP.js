a1ba3de36f3ad53394f32828f17ad233216457eb
AnshikaOel <119701265+AnshikaOel@users.noreply.github.com>
2024-01-31 16:03:42 +0530

    Update OTP.js

--- src/OTP.js
+++ src/OTP.js
@@ -4,16 +4,16 @@ import image from "./shareThought.jpg"
 
 export default function OTP() {
   const location = useLocation();
+ 
   const navigate=useNavigate();
   const  [userOTP,setuserOTP]=useState('')
   const {state:{info}}=location
-
+  // console.log("this is location "+info)
   useEffect(()=>{
-    console.log('Recieved props',info)
+    console.log('Recieved props in otp',info) 
   },[info])
   
   const handleregistration=async(e)=>{
-    console.log(info)
     try{
     const response=await fetch("http://localhost:5000/userInfo",{
         method:'POST',
@@ -21,19 +21,20 @@ export default function OTP() {
           'Content-Type':'application/json',
         },
     body:JSON.stringify(info),
-    })
- if(response.ok){
-  console.log("Data saved succcessfully on server")
- }else{
-  navigate('/registration')
-  console.log("Failed to save data server"+response.status)
- }
-}catch(err){
-    console.error(err)
-}
+    }) 
+    if(response.ok){
+      console.log("Data saved succcessfully on server")
+    }else{
+      navigate('/registration')
+      console.log("Failed to save data server"+response.status)
+    }
+    }catch(err){
+      console.error(err)
+    }
 }
   const SubmitOTP=async(e)=>{
     e.preventDefault();  
+    console.log("this is just try")
     try{
       const response=await fetch('http://localhost:5000/verifyOTP',{
         method:'POST',
@@ -44,10 +45,20 @@ export default function OTP() {
       })
       const responseData=await response.json()
       if(responseData.message=='true'){
-        // console.log("this is otp"+responseData.message)
+        console.log("this is otp--"+responseData.message)
+        //  info=JSON.stringify(info)
+        // console.log("this is OTP"+info)
+        //
         handleregistration()
-        navigate('/AccountCreated',{state:{info}})
+        let a=JSON.stringify(info)
+        let b=JSON.parse(a)
+        console.log("this is handle rehistration"+typeof(b))
+        console.log("this is handle rehistration"+b.id)
+        let id=b.id
+        // console.log("this is for accounte created")
+        navigate('/AccountCreated',{state:{id}})
       }else{
+        // console.log("this is for registration")
         navigate('/registration')
       }
     }catch(error){
